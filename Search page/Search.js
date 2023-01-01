2b62652ccc85ff5e35ab30fd02190b35b2ef91f3
Vinisha Bandara <lpbvnbandara@gmail.com>
2023-01-01 12:26:13 +0530

    #	renamed:    Images/Apartment (1).jpg -> Images/Apartment(1).jpg #	renamed:    Images/Apartment (2).jpg -> Images/Apartment(2).jpg #	renamed:    Images/Apartment (3).jpg -> Images/Apartment(3).jpg #	renamed:    Images/Apartment (4).jpg -> Images/Apartment(4).jpg #	renamed:    Images/Apartment (5).jpg -> Images/Apartment(5).jpg #	modified:   Search page/Search.css #	modified:   Search page/Search.html #	modified:   Search page/Search.js

--- Search page/Search.js
+++ Search page/Search.js
@@ -1,115 +1,112 @@
-var data = [
+var data = { 
+
+	"properties": [
 	{
-		"propertyStatus":"Rent",
-		"propertyType":"Apartment",
-		"locations":"Colombo 5",
-		"bedRooms":"3",
-		"bathrooms":"2",
-		"price":"25000",
-		"image":"/Images/Apartment (1).jpg",
+		// 1
+	"PropertyStatus":"For Rent",
+	"PropertyType":"Apartments",
+	"Locations":"Colombo 5",
+	"title":"Corsair Residencies Kassapa Road - Colombo 5",
+	"price":"$25,000.00",
+	"img":"/Images/Apartment(1).jpg",
+	"paragrap":"Kassapa Road, Jawatta, Colombo 5",
+	"beds":"3",
+	"baths":"2",
+	"sqfeet":"600 Sq Ft",
+	"pricerange":"$10,000.00 - $30,000.00",
 	},
 	{
-		"propertyStatus":"Sell",
-		"propertyType":"House",
-		"locations":"Colombo 5",
-		"bedRooms":"2",
-		"bathrooms":"2",
-		"price":"15000",
-		"image":"/Images/Apartment (2).jpg",
+		// 2
+	"PropertyStatus":"For Rent",
+	"PropertyType":"Apartments",
+	"Locations":"Colombo 5",
+	"title":"Corsair Residencies Bauddhaloka Mawatha",
+	"price":"$55,000.00",
+	"img":"/Images/Apartment(4).jpg",
+	"paragrap":"Bauddhaloka Mawatha, Colombo 5",
+	"beds":"3",
+	"baths": "2",
+	"sqfeet":"600 Sq Ft",
+	"pricerange":"$10,000.00 - $30,000.00",
+	
 	},
-
-];
-
-var PropertyStatus1 = "",
-    PropertyType1 = "",
-	Locations1 = "",
-	BedRooms1 = "",
-	Bathrooms1 = "",
-	Price1 = "",
-	Property = "";
-
-
-
-for (var i = 0; i < data.length; i++) {
-	var PropertyStatus = data[i].propertyStatus,
-	    PropertyType = data[i].propertyType,
-		Locations = data[i].locations,
-		BedRooms = data[i].bedRooms,
-		Bathrooms =data[i].bathrooms,
-		Price = data[i].price,
-		rawPrice = Price.replace("$",""),
-		rawPrice = parseInt(rawPrice.replace(",","")),
-		image = data[i].image;
-
+	{
+		// 3
+	"PropertyStatus":"For Rent",
+	"PropertyType":"Apartments",
+	"Locations":"Colombo 8",
+	"title":"Corsair Residencies Castle Street - Colombo 08",
+	"price":"$65,000.00",
+	"img":"/Images/Apartment(3).jpg",
+	"paragrap":"Castle Street,Colombo 08",
+	"beds":"5",
+	"baths": "3",
+	"sqfeet":"600 Sq Ft",
+	"pricerange":"$40,000.00 - $60,000.00",
+		
+	},
 	
-	//create product cards
-	Property += "<section class='featured' id='featured'><div class='box-container'><div class='box' data-propertyStatus='" + PropertyStatus + "'data-propertyType='" + PropertyType + "'data-location='" + Locations +"'data-bedRooms='" + BedRooms + " 'data-bathrooms='" + Bathrooms + "'data-price='" +rawPrice+ "'> <div class='image-container'><img src= ' "+ image +" '> <div class='info'><h3>3 days ago</h3> <h3>for rent</h3> </div> <div class='icons'> <a href='#' class='fas fa-film'><h3>1</h3></a><a href='#' class='fas fa-camera'> <h3>4</h3> </a> </div> </div> <div class='Feature-content'><div class='price'><h3> " +rawPrice+ " </h3> <a href='#' class='fas fa-heart'></a><a href='#' class='fas fa-envelope'> </a> <a href='#' class='fas fa-phone'> </a> </div> <div class='location'> <h3>Corsair Residencies Kassapa Road</h3> <p>Kassapa Road, Jawatta, Colombo 5</p> </div> <div class='details'> <h3> <i class='fas fa-expand'></i> 3500 sqft </h3> <h3> <i class='fas fa-bed'></i> "+ BedRooms +" </h3> <h3> <i class='fas fa-bath'></i> " + Bathrooms + " </h3> </div> <div class='buttons'> <a href='#' class='btn'> view details </a></div></div></div> </div> </section> ";
-
-
-	//create dropdown of PropertyStatus 
-	if (PropertyStatus1.indexOf("<option value='" + PropertyStatus + "'>" + PropertyStatus + "</option>") == -1) {
-		PropertyStatus1 += "<option value='" + PropertyStatus + "'>" + PropertyStatus + "</option>";
-	}
-
-	//create dropdown of PropertyType
-	if (PropertyType1.indexOf("<option value='" + PropertyType +"'>" + PropertyType + "</option>") == -1) {
-		PropertyType1 += "<option value='" + PropertyType + "'>" + PropertyType + "</option>";
-	}
-
-	//create dropdown of Locations
-	if (Locations1.indexOf("<option value='" + Locations + "'>" + Locations + "</option>") == -1) {
-		Locations1 += "<option value='" + Locations + "'>" + Locations + "</option>";
-	}
-
-	//create dropdown of BedRooms
-	if (BedRooms1.indexOf("<option value='" + BedRooms + "'>" + BedRooms + "</option>") == -1) {
-		BedRooms1 += "<option value='" + BedRooms + "'>" + BedRooms + "</option>";
-	}
-
-	//create dropdown of Bathrooms
-	if (Bathrooms1.indexOf("<option value='" + Bathrooms + "'>" + Bathrooms + "</option>") == -1) {
-		Bathrooms1 += "<option value='" + Bathrooms + "'>" + Bathrooms + "</option>";
-	}
-
-	//create dropdown of Price
-	if (Price1.indexOf("<option value='" + Price + "'>" + Price + "</option>") == -1) {
-		Price1 += "<option value='" + Price + "'>" + Price + "</option>";
-	}
-}
-
-$("#feature").html(Property);
-$("#filter-PropertyStatus").append(PropertyStatus1);
-$("#filter-PropertyType").append(PropertyType1);
-$("#filter-Locations").append(Locations1);
-$("#filter-BedRooms").append(BedRooms1);
-$("#filter-Bathrooms").append(Bathrooms1);
-$("#filter-Price").append(Price1);
-
-var filtersObject = {};
-
-//on filter change
-$(".filter").on("change",function() {
-	var filterName = $(this).data("filter"),
-		filterVal = $(this).val();
+	{
+		// 4
+	"PropertyStatus":"For Sale",
+	"PropertyType":"Apartments",
+	"Locations":"Colombo 7",
+	"title":"Corsair Residencies Barnes Place - Colombo 07",
+	"price":"$75,000.00",
+	"img":"/Images/Apartment(4).jpg",
+	"paragrap":"Barnes Place, Colombo 07",
+	"beds":"2",
+	"baths":"1",
+	"sqfeet":"600 Sq Ft",
+	"pricerange":"$40,000.00 - $60,000.00",
 	
-	if (filterVal == "") {
-		delete filtersObject[filterName];
-	} else {
-		filtersObject[filterName] = filterVal;
-	}
-	
-	var filters = "";
-	
-	for (var key in filtersObject) {
-	  	if (filtersObject.hasOwnProperty(key)) {
-			filters += "[data-"+key+"='"+filtersObject[key]+"']";
-	 	}
-	}
-	
-	if (filters == "") {
-		$("#featured").show();
-	} else {
-		$("#featured").hide();
-		$("#featured").hide().filter(filters).show();
-	}
-});
+	},
+	{
+		// 5
+	"PropertyStatus":"For Rent",
+	"PropertyType":"Apartments",
+	"Locations":"Colombo 7",
+	"title":"Corsair Residencies Malabe - Battaramulla",
+	"price":"$85,000.00",
+	"img":"/Images/Apartment(4).jpg",
+	"paragrap":"Malabe, Battaramulla",
+	"beds":"4",
+	"baths":"1",
+	"sqfeet":"600 Sq Ft",
+	"pricerange":"$70,000.00 - $100,000.00",
+	},
+	]}
+$(function() {
+	$( "#filter" ).on("click", function(){
+		
+		var propStat = $("#Property-Status").val();
+	    var propType =  $("#Property-Type1").val();
+        var location =  $("#Locations").val();
+		var bedrooms =  $("#Bedrooms").val();
+		var bathrooms = $("#Bathrooms").val();
+		// var priceranger= $ ("#Price").val();
+		
+		
+		var output="<ul>";
+		   for (var i in data.properties) {
+			   if (( propStat == data.properties[i].PropertyStatus) || (propStat=="All"))
+			   if (( propType == data.properties[i].PropertyType) || (propType=="All"))
+			   if (( location == data.properties[i].Locations))
+			   if (( bedrooms == data.properties[i].beds))
+			   if (( bathrooms == data.properties[i].baths))
+			//    if (( priceranger == data.properties[i].pricerange))
+			   			{
+							{
+								{
+									{
+										{
+											
+								
+									
+										
+				   			output+="<section class='featured'id='Placeholder'><div class='box-container'> <div class='box'> <div class='image-container'> <img src="+data.properties[i].img+"> <div class='info'> <h3>3 days ago</h3> <h3>"+data.properties[i].PropertyStatus+"</h3> </div><div class='icons'> <a href='#' class='fas fa-film'><h3>1</h3></a> <a href='#' class='fas fa-camera'><h3>4</h3></a> </div></div><div class='Feature-content'> <div class='price'> <h3>"+data.properties[i].price+"</h3> <a href='#' class='fas fa-heart'></a> <a href='#' class='fas fa-envelope'></a> <a href='#' class='fas fa-phone'></a> </div><div class='location'> <h3>"+data.properties[i].title+"</h3> <p>"+data.properties[i].paragrap+"</p></div><div class='details'> <h3> <i class='fas fa-expand'></i> 3500 sqft </h3> <h3> <i class='fas fa-bed'></i>" +data.properties[i].beds+ " beds </h3> <h3> <i class='fas fa-bath'></i>" +data.properties[i].baths+" baths </h3> </div><div class='buttons'> <a href='#' class='btn'>view details</a> </div></div></div></div></section>";
+						}  } } } } } 
+				output+="</ul>";
+				document.getElementById( "Placeholder" ).innerHTML = output;
+		   });
+	});
